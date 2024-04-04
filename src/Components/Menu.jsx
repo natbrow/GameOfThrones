e132dab560b345ad9c9e1f6c7ef8405e33e36914
paulabenitoc <paulabenitocapellan@gmail.com>
2024-04-04 17:20:47 +0200

    routes

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Menu.css';
+
+function Menu() {
+  return (
+    <nav className='nav-menu'>
+      <ul>
+        <li>
+          <NavLink exact="true" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/personajes">
+            Personajes
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/casas">
+            Casas
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/cronologia">
+            Cronología
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Menu;
---
+++ src/pages/PersonajesPage/PersonajesPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PersonajesPage() {
+  return (
+      <div>
+          <p>esto es la pagina de personajes</p> 
+    </div>
+  )
+}
