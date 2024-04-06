06f818127724d6f8094412e089e26f1b5e5bdb68
natbrow <145482189+natbrow@users.noreply.github.com>
2024-04-06 20:59:19 +0200

    personajes al detalle

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Menu.css';
+import { useTranslation } from 'react-i18next';
 
-function Menu() {
+
+function Menu({ func }) {
+  const { t } = useTranslation();
   return (
     <nav className='nav-menu'>
       <ul>
@@ -27,6 +30,10 @@ function Menu() {
           </NavLink>
         </li>
       </ul>
+      {/* <div className='container-flags'>
+        <img className="flag" src='https://www.arenaldesevilla.com/13535-large_default/pegatina-bandera-espana-sin-escudo-relieve.jpg' alt="bandera española" onClick={() => func('es')} />
+        <img className="flag" src='https://www.banderasphonline.com/wp-content/uploads/2020/05/comprar-bandera-estados-unidos-para-mastil-exterior-interior.png' alt="bandera inglesa" onClick={() => func('en')} />
+      </div> */}
     </nav>
   );
 }
--- src/pages/PersonajesPage/PersonajesPage.jsx
+++ src/pages/PersonajesPage/PersonajesPage.jsx
@@ -1,44 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import './Personajes.css'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
+import { useTranslation } from 'react-i18next';
+import i18n from 'i18next';
+import Menu from '../../components/Menu'
+
 
 export default function Personajes() {
 
   const [personajes, setPersonajes] = useState([])
   const baseUrl = 'http://localhost:3004/characters'
+  const { t } = useTranslation();
+
+  const cambiarIdioma = (idiom) => {
+    i18n.changeLanguage(idiom);
+  }
 
   const getPersonajes = async () => {
     const res = await axios.get(baseUrl)
     setPersonajes(res.data);
-    console.log(res.data);
+    // console.log(res.data);
   }
   useEffect(() => {
 
     getPersonajes();
   }, [])
   return (
-    <>
-      {/* <div>
-        <input type="text" placeholder='Buscar...' />
 
-      </div> */}
-      <div className='contenedor'>
-        {/* <div>
-          <div></div>
-        </div> */}
+    <div className='contenedor'>
+      <SimpleBar style={{ maxHeight: 800, color: 'white' }}>
         <div className='cards'>
           {personajes.map((personaje, index) =>
-            <div className='card' key={index}>
-              {/* <div className='card-img'> */}
-              <img className='t-max' src={personaje.image} alt={personaje.name} />
-              {/* </div> */}
-              <h2>{personaje.name}</h2>
-              <h3>{personaje.house}</h3>
-              <p>{personaje.parents}</p>
 
-            </div>)}
+            <figure className='card' key={index}>
+              <img className='t-max' src={personaje.image} alt={personaje.name} />
+              <figcaption>
+                <Link to={`/personajes/${personaje.id}`}>
+                  <p> {personaje.name}</p>
+                </Link>
+              </figcaption>
+            </figure>)}
         </div>
-      </div>
-    </>
+      </SimpleBar>
+    </div>
+
   )
 }
